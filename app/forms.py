63e93e2e7a1449785f53b2984659f36cde471715
dejan <dejan@lupa.technology>
2024-11-23 18:37:35 +0100

    Added validations;

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from .models import User  # Import your User model
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -13,3 +14,15 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    # Custom validator to check if email is unique
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email is already registered. Please choose a different one.')
+
+    # Custom validator to check if password is unique
+    def validate_password(self, password):
+        user = User.query.filter_by(password=password.data).first()
+        if user:
+            raise ValidationError('This password is already in use. Please choose a different one.')
