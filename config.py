a6764d49995f720a0859980fb9331108b2610a2a
Dejan <radovanovic.dejan2804@gmail.com>
2024-12-14 18:25:48 +0100

    Push;

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '2525'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:2525@localhost/flask-pwa2'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:2525@localhost/diplomski'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
